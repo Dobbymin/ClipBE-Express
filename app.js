0fe8dc5f2d15be6a066112e42df6ec379926d63c
Dobbymin <gmin0701@knu.ac.kr>
2025-09-18 17:56:45 +0000

    fix: 스웨거 문서 수정

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const createApp = async () => {
 
   const app = express();
 
+  // Swagger 설정 적용
+  setupSwagger(app);
+
   // json 포맷을 해독하기 위해 사용하는 미들웨어
   app.use(express.json());
 
@@ -32,9 +35,6 @@ const createApp = async () => {
   app.get('/', (req, res) => {
     res.send('CLIP 개발용 임시서버입니다.');
   });
-
-  // Swagger 설정 적용
-  setupSwagger(app);
 
   // 라우터 연결
   router(app);
