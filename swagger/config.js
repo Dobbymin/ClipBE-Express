eb38202f99ebea84f21f35da745518e8e2db559d
Dobbymin <gmin0701@knu.ac.kr>
2025-09-19 01:05:26 +0000

    fix: vercel 설정 수정

--- swagger/config.js
+++ swagger/config.js
@@ -34,24 +34,13 @@ const options = {
       },
     ],
   },
-  apis: ['./*.swagger.ts', './swagger/*'],
+  apis: ['./swagger/*.swagger.js'],
 };
 
 const specs = swaggerJsdoc(options);
 
 export const setupSwagger = (app) => {
-  const swaggerUiOptions = {
-    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.29.0/swagger-ui.css',
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.29.0/swagger-ui-bundle.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.29.0/swagger-ui-standalone-preset.js',
-    ],
-    swaggerOptions: {
-      url: '/swagger.json',
-    },
-  };
-
-  app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(null, swaggerUiOptions));
+  app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(specs));
 
   // JSON 명세 보기용
   app.get('/swagger.json', (_, res) => {
