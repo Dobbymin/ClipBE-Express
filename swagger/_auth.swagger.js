5aadb1f36eb8d77075d75cf1463a9a95ebef2d0f
Dobbymin <gmin0701@knu.ac.kr>
2025-09-19 01:25:06 +0000

    fix: 오류 수정

--- swagger/_auth.swagger.js
+++ swagger/_auth.swagger.js
@@ -1,6 +1,6 @@
 /**
  * @swagger
- * '/api/auth/signup':
+ * /api/auth/signup:
  *   post:
  *     summary: 회원가입
  *     description: 회원정보를 입력하여 회원가입을 진행합니다.
@@ -20,32 +20,32 @@
  *                 type: string
  *     responses:
  *       200:
- *         description: 회원가입 성공
  *         content:
  *           application/json:
  *             schema:
- *               allOf:
- *                 - $ref: '#/components/schemas/SuccessResponse'
- *                 - type: object
- *                   properties:
- *                     data:
- *                       type: object
- *                       properties:
- *                         message:
- *                           type: string
- *                           example: "회원가입에 성공하였습니다."
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "회원가입에 성공하였습니다."
  *       400:
- *         description: 잘못된 요청
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/FailResponse'
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "회원가입에 실패하였습니다."
  *       409:
- *         description: 이미 등록된 사용자
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/FailResponse'
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "이미 등록된 사용자입니다."
  */
 
 /**
@@ -68,27 +68,32 @@
  *                 type: string
  *     responses:
  *       200:
- *         description: 로그인 성공
  *         content:
  *           application/json:
  *             schema:
- *               allOf:
- *                 - $ref: '#/components/schemas/SuccessResponse'
- *                 - type: object
- *                   properties:
- *                     data:
- *                       type: object
- *                       properties:
- *                         accessToken:
- *                           type: string
- *                         refreshToken:
- *                           type: string
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "로그인에 성공하였습니다."
+ *       400:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "로그인에 실패하였습니다."
  *       404:
- *         description: 아이디 또는 비밀번호가 잘못됨
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/FailResponse'
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "아이디 또는 비밀번호가 잘못되었습니다."
  */
 
 /**
@@ -98,73 +103,119 @@
  *     summary: JWT 재발급
  *     description: 사용자의 토큰을 재발급합니다.
  *     tags: [Auth - 회원 인증 API]
+ *
  *     responses:
  *       200:
- *         description: 토큰 재발급 성공
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/SuccessResponse'
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "토큰 재발급에 성공하였습니다."
  *       400:
- *         description: 토큰 재발급 실패
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/FailResponse'
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "토큰 재발급에 실패하였습니다."
+ *
  */
 
 /**
  * @swagger
  * /api/auth/check/duplicateNickname/{nickname}:
- *   get:
+ *   post:
  *     summary: 닉네임 중복확인
  *     description: 닉네임 중복확인을 진행합니다.
  *     tags: [Auth - 회원 인증 API]
- *     parameters:
- *       - in: path
- *         name: nickname
- *         required: true
- *         schema:
- *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nickname:
+ *                 type: string
  *     responses:
  *       200:
- *         description: 사용 가능한 닉네임
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/SuccessResponse'
- *       409:
- *         description: 이미 사용중인 닉네임
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "사용할 수 있는 닉네임입니다."
+ *       400:
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/FailResponse'
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "닉네임 중복확인에 실패하였습니다."
+ *       404:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "이미 사용중인 닉네임입니다."
+ *
  */
 
 /**
  * @swagger
  * /api/auth/check/duplicateId/{userId}:
- *   get:
+ *   post:
  *     summary: 아이디 중복확인
  *     description: 회원가입시 아이디 중복확인을 진행합니다.
  *     tags: [Auth - 회원 인증 API]
- *     parameters:
- *       - in: path
- *         name: userId
- *         required: true
- *         schema:
- *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               phoneNumber:
+ *                 type: string
  *     responses:
  *       200:
- *         description: 사용 가능한 아이디
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/SuccessResponse'
- *       409:
- *         description: 이미 사용중인 아이디
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "사용할 수 있는 아이디입니다."
+ *       400:
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/FailResponse'
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "아이디 중복확인에 실패하였습니다."
+ *       404:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "이미 사용중인 아이디입니다."
+ *
  */
